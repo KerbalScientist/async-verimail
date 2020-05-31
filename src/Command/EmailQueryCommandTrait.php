7d1ade0b0f979ed8124574caa7db8798bfa296b5
Anton Balovnev <an43.bal@gmail.com>
2020-06-01 00:36:39 +0300

    Better error message for --filter option.

--- src/Command/EmailQueryCommandTrait.php
+++ src/Command/EmailQueryCommandTrait.php
@@ -9,6 +9,7 @@ namespace App\Command;
 
 use App\Verifier\VerifyStatus;
 use Aura\SqlQuery\Common\SelectInterface;
+use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -25,12 +26,21 @@ trait EmailQueryCommandTrait
     protected function configure(): void
     {
         $this->addOption('filter', 'f', InputArgument::OPTIONAL,
-            'JSON email filter', '');
+            'JSON email filter');
     }
 
     protected function initialize(InputInterface $input, OutputInterface $output): void
     {
         parent::initialize($input, $output);
+        $filter = $input->getOption('filter');
+        if (null === $filter) {
+            $filter = [];
+        } else {
+            $filter = json_decode($input->getOption('filter'));
+        }
+        if (null === $filter) {
+            throw new InvalidOptionException('Invalid JSON in --filter option: '.json_last_error_msg());
+        }
         $this->emailSelectQuery = $this->entityManager->createSelectQuery(
             json_decode($input->getOption('filter'), true)
             ?? [
